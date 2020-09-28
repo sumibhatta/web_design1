a78dc077bd39fe4146209fb8130e68fbbdf91522
sumibhatta <sumibhatta37@gmail.com>
2020-09-28 21:53:12 +0545

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function showNav() {
+    let hamelement = document.getElementById("fake");
+    if (hamelement.style.display == 'none') {
+        hamelement.style.display = 'block';
+    } else {
+        hamelement.style.display = 'none';
+    }
+}
+
+
+function clickclick() {
+    alert("Submit Button Clicked");
+
+}
